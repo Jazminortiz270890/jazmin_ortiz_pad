213944d10131d8153a2f57e57dd796e643bde022
Jazminortiz270890 <jazmin.ortiz@est.iudigital.edu.co>
2025-03-02 01:46:44 +0000

    Actualizacion 3 Jazmin_Ortiz_actividad_1

--- src/pad_2025/Jazmin_Ortiz_actividad_1.py
+++ src/pad_2025/Jazmin_Ortiz_actividad_1.py
@@ -5,17 +5,21 @@ import os
 
 class Ingestiones():
     def __init__(self):
-        self.ruta_static="src/pad_2025/static/"
+        self.ruta_static=os.path.join("src","pad_2025","static")
         os.makedirs(self.ruta_static, exist_ok=True) 
         
     def leer_api(self,url):
-        response = requests.get(url)
-        return response.json()
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"error al obtener datos de la API: {e}")
+            return None
  
     def escribir_json(self,datos,nombre_archivo="datos.json"):
         if datos is None:
             print("No hay datos para guardar.")
-           
             return 
         ruta_completa = os.path.join(self.ruta_static, nombre_archivo) 
         try:
@@ -23,7 +27,7 @@ class Ingestiones():
                 json.dump(datos, archivo, indent=4, ensure_ascii=False)
             print(f"Datos guardados en {ruta_completa}")
         except Exception as e:
-            print("Error al escribir el archivo JSON:",e) 
+            print("Error al escribir el archivo JSON: {e}") 
         
 # creacion de la instancia 
 ingestion = Ingestiones ()
